b444c4e892b12f9a95eb36635af4c0a774a6cd03
MarkBilginer <84240891+MarkBilginer@users.noreply.github.com>
2025-09-21 17:39:06 +0200

    wip(py/chunk): first visible chunk render
    - wire chunk shader + mesh builder + scene wiring
    
    - shows single white chunk (no textures yet)

--- py/scene.py
+++ py/scene.py
@@ -1,13 +1,13 @@
 from settings import *
-from meshes.quad_mesh import QuadMesh
+from world_objects.chunk import Chunk
 
-class Scene():
+class Scene:
     def __init__(self, app):
         self.app = app
-        self.quad_mesh = QuadMesh(self.app)
+        self.chunk = Chunk(self.app)
 
     def update(self):
         pass
 
     def render(self):
-        self.quad_mesh.render()
+        self.chunk.render()
---
+++ py/world_objects/chunk.py
@@ -0,0 +1,31 @@
+from numpy.ma.core import append
+from settings import *
+
+from py.meshes.chunk_mesh import ChunkMesh
+
+
+class Chunk:
+    def __init__(self, app):
+        self.app = app
+        self.voxels: np.array = self.build_voxels()
+        self.mesh: ChunkMesh = None
+        self.build_mesh()
+
+    def build_mesh(self):
+        self.mesh = ChunkMesh(self)
+
+    def render(self):
+        self.mesh.render()
+
+    def build_voxels(self):
+        # empty chunk
+        # voxels consists of voxel ids 0...255 and their value showing voxel type where voxel value 0 is empty space
+        voxels = np.zeros(CHUNK_VOL, dtype=np.uint8)
+
+        # fill chunk
+        for x in range(CHUNK_SIZE):
+            for z in range(CHUNK_SIZE):
+                for y in range(CHUNK_SIZE):
+                    # we give here all voxels a value of 1
+                    voxels[x + CHUNK_SIZE * z + CHUNK_AREA * y] = 1
+        return voxels
